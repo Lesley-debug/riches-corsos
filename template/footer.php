184e632d9b97ac764381c03fb125891cc3f84968
Lesley Tabi <esanglesley@gmail.com>
2026-08-10 11:22:59 +0100

    feat: add Tawk.to live chat widget to all pages

--- template/footer.php
+++ template/footer.php
@@ -64,6 +64,19 @@
                     ], JSON_HEX_TAG | JSON_HEX_AMP); ?>;
 </script>
 <script src="<?= $basePath; ?>/assets/js/site.js" defer></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a79a52345989d1d4159ab60/1jvliq5ij';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
